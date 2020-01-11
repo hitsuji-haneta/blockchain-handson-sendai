dff24a5b6e282b418d4b63c645d93b2f23cc82dd
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2020-01-11 23:07:56 +0900

    Make an input form and a submit button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h1>{{message}}</h1>
+    <input type="text" v-on:input="changeName" />
+    <button v-on:click="setName">名前を登録</button>
+    <p>{{name}}</p>
   </div>
 </template>
 
@@ -11,11 +14,20 @@ export default {
   name: "app",
   data() {
     return {
-      message: ""
+      message: "",
+      name: ""
     };
   },
   mounted: async function() {
     this.message = await contract.methods.greet().call();
+  },
+  methods: {
+    setName: function() {
+      console.log(this.name);
+    },
+    changeName: function(event) {
+      this.name = event.target.value;
+    }
   }
 };
 </script>
